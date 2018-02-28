993a57bbf4b771104e862a7d1fa0e85bbe22e47b
hsalkaabi <hsalkaabi@mail.csuchico.edu>
2018-02-27 20:46:23 -0800

    arrays

--- main.cpp
+++ main.cpp
@@ -34,5 +34,16 @@ int main()
   }
 }  while(input!='q' && input!='Q');
 
+    cout<<"\n== Items to buy ==\n";
+    for( int numItems=0; numItems<5; numItems++)
+    {
+    cout<< numItems+1;
+
+    if(list[numItems]!="")
+    {
+    cout<<" "<<list[numItems]<<endl;
+    }
+   
+}    
   return 0;
 }
